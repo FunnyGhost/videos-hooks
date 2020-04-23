79bb142fbd0e9e419533110838c7e4ca95afcf32
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-23 17:49:42 +0200

    Update the state with the youtube search response

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get("/search", { params: { q: term } });
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", { params: { q: term } });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
