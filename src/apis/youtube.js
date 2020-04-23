0dcd6285351c61d34944050c8084de6ea7b27597
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-23 17:20:00 +0200

    Created axios for youtube

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 const KEY = "AIzaSyC1sk_HhyQQ9uhj4dzr0fvY8O6jJnTkNSI";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    key: KEY,
+    part: "snippet",
+    maxResults: 5,
+    type: "video",
+  },
+});
