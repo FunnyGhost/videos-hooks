4652a288f45fbd1a4c65982272c780951ee7c425
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-24 15:23:06 +0200

    Show the video thumbnail and title

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const VideoItem = ({ video }) => {
-  return <div>Video Item</div>;
+  return (
+    <div>
+      <p>{video.snippet.title}</p>
+      <img src={video.snippet.thumbnails.medium.url} alt=""></img>
+    </div>
+  );
 };
 
 export default VideoItem;
