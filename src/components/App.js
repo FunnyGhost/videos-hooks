aedfc7af9e894dd0ab62908799013c01d95d3f04
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-24 15:13:08 +0200

    VideosList Component Genesis

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = { videos: [] };
@@ -14,8 +15,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos}></VideoList>
       </div>
     );
   }
