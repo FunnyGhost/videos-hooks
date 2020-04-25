7fbf887b2f4dcc1f04ecd96c7ab41872b82234b5
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-25 19:13:06 +0200

    Implemented video selection mechanism

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedItems = videos.map((video) => (
-    <VideoItem video={video}></VideoItem>
+    <VideoItem video={video} onVideoSelect={onVideoSelect}></VideoItem>
   ));
 
   return <div className="ui relaxed divided list ">{renderedItems}</div>;
