a53425d782b1347a721949b8562395672ddd3510
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-26 15:05:31 +0200

    Added key to VideoItem

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,11 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedItems = videos.map((video) => (
-    <VideoItem video={video} onVideoSelect={onVideoSelect}></VideoItem>
+    <VideoItem
+      video={video}
+      onVideoSelect={onVideoSelect}
+      key={video.id.videoId}
+    ></VideoItem>
   ));
 
   return <div className="ui relaxed divided list ">{renderedItems}</div>;
