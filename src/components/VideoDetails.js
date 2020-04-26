9824bdf8cc59fabf625c0e91bd0d0aae824104a3
Catalin Ciubotaru <catalin.ciubotaru@snappcar.nl>
2020-04-26 15:02:22 +0200

    Added youtube iframe for the video

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -5,11 +5,18 @@ const VideoDetails = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
-    <div className="ui segment">
-      <div className="header">
-        <h4>{video.snippet.title}</h4>
-        <p>{video.snippet.description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} title={video.snippet.title}></iframe>
+      </div>
+      <div className="ui segment">
+        <div className="header">
+          <h4>{video.snippet.title}</h4>
+          <p>{video.snippet.description}</p>
+        </div>
       </div>
     </div>
   );
